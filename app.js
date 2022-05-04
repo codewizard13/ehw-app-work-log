b86d295ba3f9ec54aa9a427a511859d6d159187f
Eric L. Hepperle <codewizard13@gmail.com>
2022-05-04 11:55:07 -0500

    UPDATE app.js:  ADD UI vars

--- app.js
+++ app.js
@@ -1,2 +1,8 @@
 // app.js
 
+// DEFINE UI VARS
+const topHeader = document.querySelector('h1:first-of-type')
+const form = document.getElementById('entry-form')
+const inputSubject = document.getElementById('subject')
+const inputDetails = document.getElementById('details')
+const submitBtn = document.getElementById('submit-note')
