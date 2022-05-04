a8d5281916a6007183e7bac426e5c68b3169143b
Eric L. Hepperle <codewizard13@gmail.com>
2022-05-04 12:14:31 -0500

    UPDATE app.js:  BUILD addEntry() func

--- app.js
+++ app.js
@@ -11,3 +11,30 @@ const submitBtn = document.getElementById('submit-note')
 const filter = document.getElementById('filter')
 const resultsList = document.querySelector('.collection')
 
+// Load all event listeners
+loadEventListeners()
+
+/** Load event listeners */
+function loadEventListeners() {
+  // Add log entry event
+  form.addEventListener('submit', addEntry)
+}
+
+/** Add Entry */
+function addEntry(e) {
+  alert('Add an Entry')
+
+  // Create LI element
+  const li = document.createElement('li')
+  // Add class
+  li.className = 'collection-item' // for materialize
+  // Create text node and append to LI
+  li.appendChild(document.createTextNode(inputSubject.value))
+  // Create a new link element for DELETE x icon
+  const delLink = document.createElement('a')
+  // Add class
+  delLink.className = 'delete-item'
+
+
+  e.preventDefault()
+}
