c8313bbd9b8f24db60893399504731a19a88d131
Eric L. Hepperle <codewizard13@gmail.com>
2022-05-04 12:49:32 -0500

    UPDATE index.html:  ADD FontAwesome

--- app.js
+++ app.js
@@ -22,19 +22,24 @@ function loadEventListeners() {
 
 /** Add Entry */
 function addEntry(e) {
-  alert('Add an Entry')
 
   // Create LI element
   const li = document.createElement('li')
   // Add class
-  li.className = 'collection-item' // for materialize
+  li.className = 'collection-item list-group-item mt-3' // for materialize
   // Create text node and append to LI
   li.appendChild(document.createTextNode(inputSubject.value))
   // Create a new link element for DELETE x icon
   const delLink = document.createElement('a')
   // Add class
-  delLink.className = 'delete-item'
+  delLink.className = 'delete-item secondary-content ' // materialize classes
+  // Add icon HTML
+  delLink.innerHTML = '<i class="fa fa-remove justify-self-end"></i>' // requires FontAwesome
+  // Append delete link to li
+  li.appendChild(delLink)
 
+  // APPEND LI TO UL
+  resultsList.appendChild(li)
 
   e.preventDefault()
 }
