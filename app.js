2f827e89074a80cadba21ffbcadd854bc637bfd3
Eric L. Hepperle <codewizard13@gmail.com>
2022-05-05 07:27:15 -0500

    ADD junk.htm:  TESTING card layout with Flexbox

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ function loadEventListeners() {
   form.addEventListener('submit', addEntry)
 }
 
-/** Add Entry */
+/** Add Entry Card*/
 function addEntry(e) {
 
   // Create LI element
@@ -41,40 +41,21 @@ function addEntry(e) {
   delLink.className = 'delete-item secondary-content' // materialize classes
   // Add icon HTML
   delLink.innerHTML = '<i class="fa fa-remove"></i>' // requires FontAwesome
-  
-  /* TESTING BOOSTRAP CARD LITERAL */
-/*  test_card_html = `
-  <div class="card" style="width: 90%;">
-  <img class="card-img-top" src="img/test.jpg" alt="Card image cap" style="">
-  <div class="card-body">
-    <h5 class="card-title text-left">${inputSubject.value}</h5>
-    <p class="card-text text-left">${inputDetails.value}</a>
-  </div>
-  </div>`
-*/
 
-  // Test different version of card literal
+  // Entry Card template literal
   let test_card_html = `
-  <div class="card-body">
+  <div>
     <h5 class="card-title text-left">${inputSubject.value}</h5>
     <p class="card-text text-left">${inputDetails.value}</a>
-  </div>`
-
-
-  let actual_html = `<li class="collection-item card list-group-item mt-3 d-flex justify-content-between"><div>
-  <div class="card" style="width: 90%;">
-  <div class="card-body">
-    <h5 class="card-title text-left">Thank You! Job reference for O'Reilly</h5>
-    <p class="card-text text-left"></p><p>When I shrink the size of the page to preview how it would look in a mobile device the text nicely adjusts itself, however the images don't. Now i have about 3000 such images and if there is an attribute that I have to apply to every individual image tag, that won't be feasible.</p>
-
-<p>Is there any other way using CSS or bootstrap or javascript where I can reduce the size of the image for a device size, keeping the image aspect ratio intact ?</p>
   </div>
-  </div><a class="delete-item secondary-content"><i class="fa fa-remove" aria-hidden="true"></i></a></div></li>`
+  <a class="delete-item secondary-content"><i class="fa fa-remove" aria-hidden="true"></i></a>
+  `
 
   const testCrd = document.createElement('div')
+  testCrd.className = 'card-body'
   testCrd.innerHTML = test_card_html
   console.log(testCrd)
-  testCrd.appendChild(delLink)
+  // testCrd.appendChild(delLink)
 
 
   // Append CHILD ELEMENTS to LI
